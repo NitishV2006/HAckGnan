3cf7a61939a891c07f6c2a890cc596f38aed9376
NitishV2006 <nitishvellanki@outlook.com>
2025-09-14 05:48:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 8080,
-    allowedHosts: ["hackgnan.onrender.com"],
+    allowedHosts: ["https://hackgnan-1.onrender.com"],
   },
   preview: {
     port: 8080,
